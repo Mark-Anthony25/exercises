df2025c65708e9a628ab662fe102b13efc759825
Mark-Anthony25 <98404531+Mark-Anthony25@users.noreply.github.com>
2025-03-08 18:42:53 +0800

    added a new class 'Easy'

---
+++ Exercises/src/Easy.java
@@ -0,0 +1,28 @@
+
+public class Easy {
+
+
+
+	public String frontBack(String str) {
+		int s = str.length();
+		int a = s - 1;
+		if (s <= 1) {
+			return str;
+		}
+		String mid = str.substring(1, a);
+		return "" + str.charAt(a) + mid + str.charAt(0);
+	}
+
+	public String front3(String str) {
+
+		if (str.length() <= 3) {
+			for (int num = 0; num < 3; num++) {
+
+			}
+			return str + str + str;
+		}
+		String three = str.substring(0, 3);
+		return three + three + three;
+	}
+
+}
--- Exercises/src/Solution.java
+++ Exercises/src/Solution.java
@@ -2,27 +2,11 @@
 public class Solution {
 
 	public static void main(String[] args) {
-		int y = diff21(22);
-		System.out.println(y);
-		String greet = "Hello, World";
-		if (greet.contains("World")) {
-			System.out.println("Hello, World");
-		} else {
-			System.out.println("hindi nagana");
-		}
-		// System.out.println("Hello, World!");
-		// TODO Auto-generated method stub
-
+		Easy easy = new Easy();
+		System.out.println(easy.frontBack("Hello"));
+		System.out.println(easy.front3("Java"));
 	}
+		
 
-	public static int diff21(int n) {
-		int dif = Math.abs(n - 21);
-
-		if (dif <= 21) {
-			return dif;
-		} else if (n > 21) {
-			return dif * 2;
-		}
-		return dif;
-	}
+	
 }
