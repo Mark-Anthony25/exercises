79da2e7ee84198991858c63ee12afdad8891a117
Mark-Anthony25 <98404531+Mark-Anthony25@users.noreply.github.com>
2025-03-10 12:43:56 +0800

    added a new solution in 'Easy' class

--- Exercises/src/Solution.java
+++ Exercises/src/Solution.java
@@ -6,6 +6,6 @@ public class Solution {
 		System.out.println(easy.frontBack("Hello"));
 		System.out.println(easy.front3("Java"));
 		System.out.println(easy.missingChar("kitten", 2));
-		System.out.println(easy.backAround("Cat"));
+		System.out.println(easy.backAround("HERE"));
 	}
 }
