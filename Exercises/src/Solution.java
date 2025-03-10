950e53649cf6584697cdc3033e958f7fa02296e6
Mark-Anthony25 <98404531+Mark-Anthony25@users.noreply.github.com>
2025-03-10 14:52:30 +0800

    updated the string output of the 'backAround' method

--- Exercises/src/Solution.java
+++ Exercises/src/Solution.java
@@ -1,11 +1,10 @@
 
 public class Solution {
-
 	public static void main(String[] args) {
 		Easy easy = new Easy();
 		System.out.println(easy.frontBack("Hello"));
 		System.out.println(easy.front3("Java"));
 		System.out.println(easy.missingChar("kitten", 2));
-		System.out.println(easy.backAround("TRY"));
+		System.out.println(easy.backAround("TRY It"));
 	}
 }
