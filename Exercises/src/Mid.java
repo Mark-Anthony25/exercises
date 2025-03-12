c3c201f2ca866d4f812274cc331cb20601dc1d55
Mark-Anthony25 <98404531+Mark-Anthony25@users.noreply.github.com>
2025-03-12 23:49:17 +0800

    added a new method 'isOdd' in 'Mid' class

--- Exercises/src/Mid.java
+++ Exercises/src/Mid.java
@@ -17,6 +17,47 @@ public class Mid {
             return -1.0;
         }
         return result;
-
     }
+
+    public static void printNumberInWord(int number) {
+        switch (number) {
+            case 0 -> System.out.println("ZERO");
+            case 1 -> System.out.println("ONE");
+            case 2 -> System.out.println("TWO");
+            case 3 -> System.out.println("THREE");
+            case 4 -> System.out.println("FOUR");
+            case 5 -> System.out.println("FIVE");
+            case 6 -> System.out.println("SIX");
+            case 7 -> System.out.println("SEVEN");
+            case 8 -> System.out.println("EIGHT");
+            case 9 -> System.out.println("NINE");
+            case 10 -> System.out.println("TEN");
+            default -> System.out.println("OTHERS");
+        }
+    }
+
+
+    public static boolean isOdd(int number) {
+        if (number < 0) {
+            return false;
+        }
+        return number % 2 != 0;
+    }
+
+    public static int sumOdd(int start, int end) {
+        if (start > end || start < 0 || end < 0) {
+            return -1;
+        }
+        int result = 0;
+        for (int i = start; i <= end; i++) {
+            if (isOdd(i)) {
+                result += i;
+            }
+
+        }
+        return result;
+    }
+
+
+    
 }
